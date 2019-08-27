338af40beb0383cf2e6fbbe1bef14035e76a5ab9
“xuhongv-macPro” <xuhongv@yeah.net>
2019-08-27 22:29:38 +0800

    完善输入定位
    完善输入定位

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,7 @@
 import mqtt from '../../utils/mqtt/mqtt.min.js';
 import QQMapWX from '../../utils/map/qqmap-wx-jssdk.min.js';
 
-//连接的服务器域名，注意格式！！！
+//连接的服务器域名，注意格式！！！ 867959033013358
 const host = 'wxs://www.xuhonys.cn/mqtt';
 var qqmapsdk;
 
@@ -11,6 +11,8 @@ var qqmapsdk;
 // 微信 腾讯地图开发文档：https://developers.weixin.qq.com/miniprogram/dev/component/map.html
 Page({
   data: {
+    deviceIMEI:'',
+    hiddenmodalput: true,
     client: null,
     //记录重连的次数
     reconnectCounts: 0,
@@ -18,9 +20,9 @@ Page({
     //MQTT连接的配置
     options: {
       protocolVersion: 4, //MQTT连接协议版本
-      clientId: 'miniTest2',
+      clientId: '',
       clean: true,
-      password: 'xuhong123',
+      password: 'admin',
       username: 'admin',
       reconnectPeriod: 1000, //1000毫秒，两次重新连接之间的间隔
       connectTimeout: 30 * 1000, //1000毫秒，两次重新连接之间的间隔
@@ -46,10 +48,55 @@ Page({
       }
     }],
   },
-  onClickInput: function() {
-
+  inputDeviceIMEI(e) {
+    this.setData({
+      deviceIMEI: e.detail.value
+    })
   },
-  onClickScan: function() {
+  //弹窗确认点击事件回调
+  confirm() {
+    this.setData({
+      hiddenmodalput: true
+    })
+
+    let that = this;
+
+    that.data.client.subscribe('/A9g/' + that.data.deviceIMEI + '/update', function (err, granted) {
+      if (!err) {
+        that.setData({
+          devSubTopic: '/A9g/' + that.data.deviceIMEI + '/get'
+        })
+        wx.showToast({
+          title: '正在获取【' + that.data.deviceIMEI + "】的位置",
+          icon: 'none',
+          duration: 1000
+        })
+
+        that.setData({
+          deviceIMEI: ''
+        })
+
+        that.onClickRefresh()
+
+      } else {
+        console.log('订阅报错：' + err)
+      }
+    })
+
+   
+    
+  },
+  cancel() {
+    this.setData({
+      hiddenmodalput: true
+    })
+  },
+  onClickInput(){
+    this.setData({
+      hiddenmodalput: false
+    });
+  },
+  onClickScan: function () {
     let that = this;
     if (this.data.client && this.data.client.connected) {
       wx.scanCode({
@@ -95,15 +142,17 @@ Page({
     }
 
   },
-  onClickRefresh: function() {
+  onClickRefresh: function ()  {
     console.log("this.data.devSubTopic:" + this.data.devSubTopic)
     if (this.data.client && this.data.client.connected) {
-      if (this.data.devSubTopic){
-      this.data.client.publish(this.data.devSubTopic, 'i am  from wechat msg');
-      wx.showToast({
-        title: '发布成功' 
-      })
-      }else
+      let data = new Object;
+      data.cmd = 'refresh';
+      if (this.data.devSubTopic) {
+        this.data.client.publish(this.data.devSubTopic, JSON.stringify(data),{qos:1});
+        wx.showToast({
+          title: '发布成功'
+        })
+      } else
         wx.showToast({
           title: '请先添加设备',
           icon: 'none',
@@ -123,7 +172,7 @@ Page({
 
     var timestamp = (new Date()).valueOf();
     this.setData({
-      clientID: "WC-"+timestamp
+      'options.clientId': "WC-" + timestamp
     })
 
     //开始连接
@@ -150,7 +199,12 @@ Page({
             icon: 'none',
             duration: 2000
           })
-        }
+        }else
+          wx.showToast({
+            title: '设备定位中，请稍后重试..',
+            icon: 'none',
+            duration: 2000
+          })
       }
     })
     //服务器连接异常的回调
@@ -223,8 +277,8 @@ Page({
           }
         });
 
-        console.log(" res.data.locations[0].lat:" + res.data.locations[0].lat);
-        console.log(" longitude: res.data.locations[0].lng:" + res.data.locations[0].lng);
+        //console.log(" res.data.locations[0].lat:" + res.data.locations[0].lat);
+        //console.log(" longitude: res.data.locations[0].lng:" + res.data.locations[0].lng);
 
       },
       fail: function(err) {}, //请求失败
